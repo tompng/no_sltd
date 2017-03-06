fc9a13ed73ab7ce1972da245080ea53552098de1
tompng <tomoyapenguin@gmail.com>
2017-03-07 02:21:14 +0900

    use thread_local(not fiber_local)

--- recursive.rb
+++ recursive.rb
@@ -4,11 +4,6 @@ module StackLevelSuperdeep
   CONTINUE = Object.new
   THREAD_LOCAL_KEY = :stack_level_superdeep_runner
 
-  def self.fiber_safe_thread_local
-    @thread_locals ||= {}
-    @thread_locals[Thread.current.__id__] ||= {}
-  end
-
   class Runner
     attr_reader :result
     def initialize
@@ -46,14 +41,14 @@ module StackLevelSuperdeep
 
     def self.execute &block
       runner = Runner.new
-      StackLevelSuperdeep.fiber_safe_thread_local[THREAD_LOCAL_KEY] = runner
+      Thread.current.thread_variable_set(THREAD_LOCAL_KEY, runner)
       runner.start &block
     ensure
-      StackLevelSuperdeep.fiber_safe_thread_local[THREAD_LOCAL_KEY] = nil
+      Thread.current.thread_variable_set(THREAD_LOCAL_KEY, nil)
     end
 
     def self.current
-      StackLevelSuperdeep.fiber_safe_thread_local[THREAD_LOCAL_KEY]
+      Thread.current.thread_variable_get(THREAD_LOCAL_KEY)
     end
   end
 
@@ -70,6 +65,23 @@ def recursive &block
   StackLevelSuperdeep.recursive &block
 end
 
+
+# test
+
+def sum n
+  return 1 if n == 1
+  sum(n-1) + n
+end
+
+def sum_ok n
+  return 1 if n == 1
+  recursive{sum_ok(n-1)} + n
+end
+
+p Benchmark.measure{p sum_ok 10000}.real
+p Benchmark.measure{p sum_ok 40000}.real
+p Benchmark.measure{p sum_ok 160000}.real
+
 def fib_ok a, memo={0 => 0, 1 => 1}
   return memo[a] if memo[a]
   memo[a-1] ||= recursive { fib_ok a-1, memo }
@@ -84,19 +96,6 @@ def fib_err a, memo={0 => 0, 1 => 1}
   memo[a-1] + memo[a-2]
 end
 
-def sum n
-  return 1 if n == 1
-  sum(n-1) + n
-end
-
-def sum_ok n
-  return 1 if n == 1
-  recursive{sum_ok(n-1)} + n
-end
-
-p Benchmark.measure{p sum_ok 20000}.real
-p Benchmark.measure{p sum_ok 50000}.real
-
 raise unless 100.times.all?{|i|fib_ok(i)==fib_err(i)}
-p fib_ok(10000).to_s.size #=> 2090
-p fib_err(10000) #=> stack level too deep
+p fib_ok(100000).to_s.size #=> 20899
+p fib_err(100000) #=> stack level too deep
