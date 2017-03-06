26f0f6789d5bb0245891b614a39a04fdff162a5e
tompng <tomoyapenguin@gmail.com>
2017-03-07 02:38:39 +0900

    split bench

--- stack_level_superdeep.rb
+++ stack_level_superdeep.rb
@@ -1,5 +1,3 @@
-require 'pry'
-require 'benchmark'
 module StackLevelSuperdeep
   CONTINUE = Object.new
   THREAD_LOCAL_KEY = :stack_level_superdeep_runner
@@ -65,40 +63,3 @@ end
 def recursive &block
   StackLevelSuperdeep.recursive &block
 end
-
-
-# test
-
-def sum n
-  return 1 if n == 1
-  sum(n-1) + n
-end
-
-def sum_safe n
-  return 1 if n == 1
-  recursive{sum_safe(n-1)} + n
-end
-
-p Benchmark.measure{p sum 10000}.real
-p Benchmark.measure{p sum_safe 10000}.real
-p Benchmark.measure{p sum_safe 40000}.real
-#p Benchmark.measure{p sum 40000}.real #=> stack level too deep
-p Benchmark.measure{p sum_safe 160000}.real
-
-def fib_safe a, memo={0 => 0, 1 => 1}
-  return memo[a] if memo[a]
-  memo[a-1] ||= recursive { fib_safe a-1, memo }
-  memo[a-2] ||= recursive { fib_safe a-2, memo }
-  memo[a-1] + memo[a-2]
-end
-
-def fib a, memo={0 => 0, 1 => 1}
-  return memo[a] if memo[a]
-  memo[a-1] ||= fib a-1, memo
-  memo[a-2] ||= fib a-2, memo
-  memo[a-1] + memo[a-2]
-end
-
-raise unless 100.times.all?{|i|fib_safe(i)==fib(i)}
-p fib_safe(100000).to_s.size #=> 20899
-p fib(100000) #=> stack level too deep
