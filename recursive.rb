ebf5f52f9758ba3d0a1ec28bd3cf0eba6fd50660
tompng <tomoyapenguin@gmail.com>
2017-03-07 00:59:14 +0900

    fib example

--- recursive.rb
+++ recursive.rb
@@ -1,16 +1,4 @@
 require 'pry'
-def hoge2 i
-  return i if i<=1
-  hoge2(i-1)+1
-end
-
-def hoge i
-  return i if i<=1
-  a = recursive { hoge i-1 }
-  # b = recursive { hoge i-2 }
-  # a+b
-  a+1
-end
 
 def recursive &block
   unless @requests
@@ -41,5 +29,20 @@ ensure
   @result = nil
 end
 
-p hoge 20000
-p hoge2 20000
+def fib_ok a, memo={0 => 0, 1 => 1}
+  return memo[a] if memo[a]
+  memo[a-1] ||= recursive { fib_ok a-1, memo }
+  memo[a-2] ||= recursive { fib_ok a-2, memo }
+  memo[a-1] + memo[a-2]
+end
+
+def fib_err a, memo={0 => 0, 1 => 1}
+  return memo[a] if memo[a]
+  memo[a-1] ||= fib_err a-1, memo
+  memo[a-2] ||= fib_err a-2, memo
+  memo[a-1] + memo[a-2]
+end
+
+raise unless 100.times.all?{|i|fib_ok(i)==fib_err(i)}
+p fib_ok(10000) #=> 2090桁の数字
+p fib_err(10000) #=> stack level too deep
