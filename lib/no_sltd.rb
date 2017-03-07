72155a81fea874258f4382d040f6c3eb92468d71
tompng <tomoyapenguin@gmail.com>
2017-03-08 03:06:39 +0900

    Fiber.new{}.resume

--- lib/no_sltd.rb
+++ lib/no_sltd.rb
@@ -1,67 +1,24 @@
 require "no_sltd/version"
 
 module NoSLTD
-  CONTINUE = Object.new
-  THREAD_LOCAL_KEY = :no_sltd_runner
+  THREAD_LOCAL_KEY = :no_sltd_stack_level
 
-  class Runner
-    attr_reader :result
-    def initialize
-      @fibers = []
-      @stack_level = 0
-    end
-
-    def << fiber
-      @fibers << fiber
-    end
-
-    def start &block
-      self << Fiber.new { block.call }
-      until @fibers.empty? do
-        f = @fibers.last
-        next unless f
-        res = f.resume
-        if res != CONTINUE
-          @result = res
-          @fibers.pop
-        end
-      end
-      @result
-    end
-
-    def with_stack_level
-      level = @stack_level
-      @stack_level += 1
-      begin
-        yield level
-      ensure
-        @stack_level = level
-      end
-    end
-
-    def self.execute &block
-      runner = Runner.new
-      Thread.current.thread_variable_set(THREAD_LOCAL_KEY, runner)
-      runner.start(&block)
+  def self.with_stack_level
+    level = (Thread.current[THREAD_LOCAL_KEY] || 0)
+    Thread.current[THREAD_LOCAL_KEY] = level + 1
+    begin
+      yield level
     ensure
-      Thread.current.thread_variable_set(THREAD_LOCAL_KEY, nil)
-    end
-
-    def self.current
-      Thread.current.thread_variable_get(THREAD_LOCAL_KEY)
+      Thread.current[THREAD_LOCAL_KEY] = level
     end
   end
 
-  def self.recursive &block
-    runner = Runner.current
-    return Runner.execute(&block) unless runner
-    runner.with_stack_level do |level|
-      if level % 64 != 0
+  def self.recursive
+    with_stack_level do |level|
+      if level < 64
         yield
       else
-        runner << Fiber.new(&block)
-        Fiber.yield CONTINUE
-        runner.result
+        Fiber.new { yield }.resume
       end
     end
   end
